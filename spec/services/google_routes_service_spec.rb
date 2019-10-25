d2593901efdc7557228e2895b367d439a2f13ea9
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:33:05 -0600

    Create test body to initial connection without errors

--- spec/services/google_routes_service_spec.rb
+++ spec/services/google_routes_service_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "google routes api test" do
   describe "get some api data?" do
     it "gets some data that is not an error" do
-
+      service = GoogleRouteService.new
+      connected = service.get_a_basic_connection
+      
     end
   end
 end
