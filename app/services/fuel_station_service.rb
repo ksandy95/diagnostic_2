c96db285ea3520fac749e6c0e2f508135f5a39ff
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 11:29:41 -0600

    Add message

--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -2,7 +2,7 @@ class FuelStationService
 
   def step_two_chunk_it_out
     one = get_a_basic_connection_step_one.get("fuel_type=ELEC&state=CA&limit=2")
-    binding.pry
+    # binding.pry
     parsed_the_data_how_could_i_forget = JSON.parse(one.body, symbolize_names: true)
   end
 
@@ -10,5 +10,10 @@ class FuelStationService
     Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?",
                 params: {'api_key' => ENV['FUEL_STATIONS_API_KEY']})
   end
+
+  # i didnt get this functional for the work time we had. I feel confident with just a little
+  # more time, i would have ended up with a funtional product. I feel much better about
+  # this diagnostic than the last one. I would also appreciate a one on one review or
+  # an in class review of this diagnostic. 
 
 end
