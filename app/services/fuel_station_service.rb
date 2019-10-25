99b158ab9ae425b6d001549a3fec84844ce2ce5f
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:57:19 -0600

    Change methods for continued separation

--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -1,11 +1,12 @@
 class FuelStationService
 
-  def step_two_chunk_it_out
-    get_a_basic_connection_step_one.get("fuel_type=ELEC&state=CA&limit=2&api_key=#{ENV['FUEL_STATIONS_API_KEY']}")
+  def step_two_chunk_it_out(state)
+    get_a_basic_connection_step_one.get("fuel_type=ELEC&state=#{state}")
   end
 
   def get_a_basic_connection_step_one
-    Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?")
+    Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1.json?",
+                params: {'api_key' => ENV['FUEL_STATIONS_API_KEY']})
   end
 
 end
