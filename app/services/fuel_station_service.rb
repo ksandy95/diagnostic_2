6709d4473d10bc1a9bb160f6def329f8b503569e
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:44:00 -0600

    Chunk out fuel station service for second step

--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -1,7 +1,11 @@
 class FuelStationService
 
+  def step_two_chunk_it_out
+    get_a_basic_connection_step_one.get("fuel_type=ELEC&state=CA&limit=2&api_key=#{ENV['FUEL_STATIONS_API_KEY']}")
+  end
+
   def get_a_basic_connection_step_one
-    Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?fuel_type=ELEC&state=CA&limit=2&api_key=#{ENV['FUEL_STATIONS_API_KEY']}")
+    Faraday.new("https://developer.nrel.gov/api/alt-fuel-stations/v1.json?")
   end
 
 end
