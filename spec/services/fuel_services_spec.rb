186b8a8f30916fbd2f16000288937534d9b62158
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:33:50 -0600

    update test

--- spec/services/fuel_services_spec.rb
+++ spec/services/fuel_services_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe "fuel service api test" do
   describe "get some api data?" do
     it "it returns something that is not an error" do
       service = FuelStationService.new
-      service.get_a_basic_connection_step_one
-      binding.pry
+      connected = service.get_a_basic_connection_step_one
+  
     end
   end
 end
