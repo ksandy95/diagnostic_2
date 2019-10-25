121c1bb2780176e267b16aa58f0ad57b65886852
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 11:19:06 -0600

    Update test for google api call for broken out functionality

--- spec/services/google_routes_service_spec.rb
+++ spec/services/google_routes_service_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe "google routes api test" do
     it "gets some data that is not an error" do
       service = GoogleRouteService.new
       connected = service.get_a_basic_connection
-      
+      origin = "1331 17th St, Denver, CO 80202"
+      destination = 
+      step_two_is_working = service.lets_chunk_this(origin, destination)
     end
   end
 end
