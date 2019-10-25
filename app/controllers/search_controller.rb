ec7e823555bd539f75eba5411c2d95cbf48bd21d
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:45:06 -0600

    anticipate next method needed to bring both services together

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,4 +4,9 @@ class SearchController < ApplicationController
     @station = "something_will_go_here_some_time"
   end
 
+  def search_station(location)
+    service = FuelStationService.new
+    
+  end
+
 end
