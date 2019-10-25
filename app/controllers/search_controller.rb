ae9f6b7203f31680871fae39b631d47eb2e41e45
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:56:36 -0600

    Create method to get location data from the frop down selected, change search method for ease of use for to get closer to step two completion

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,19 @@
 class SearchController < ApplicationController
 
   def index
+    search_for_station
     @station = "something_will_go_here_some_time"
   end
 
-  def search_station(location)
-    service = FuelStationService.new
+  def get_loctaion_data
+    params[:location]
+  end
+
+  def search_for_station
+    get_loctaion_data
     
+    service = FuelStationService.new
+
   end
 
 end
