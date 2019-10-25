507d7a6940d52383476b01dc94dd259291b9917d
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 10:44:24 -0600

    Chunk out google service for second step

--- app/services/google_route_service.rb
+++ app/services/google_route_service.rb
@@ -1,7 +1,11 @@
 class GoogleRouteService
 
+  def lets_chunk_this(origin, destination)
+    get_a_basic_connection.get("origin=#{origin}&destination=#{destination}&key=#{ENV['GOOGLE_API_KEY']}")
+  end
+
   def get_a_basic_connection
-    Faraday.get("https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood&key=#{ENV['GOOGLE_API_KEY']}")
+    Faraday.new("https://maps.googleapis.com/maps/api/directions/json?")
   end
 
 end
