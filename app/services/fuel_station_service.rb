0267292e483ccc49a2e4ea85344b6e2a335b5dbe
Kelly Sandeson <kellysandeson@gmail.com>
2019-10-25 11:21:46 -0600

    How could i forget my JSON  step no wonder i was getting stuck where i was with my tests

--- app/services/fuel_station_service.rb
+++ app/services/fuel_station_service.rb
@@ -1,7 +1,9 @@
 class FuelStationService
 
   def step_two_chunk_it_out
-    get_a_basic_connection_step_one.get("fuel_type=ELEC&state=CA&limit=2")
+    one = get_a_basic_connection_step_one.get("fuel_type=ELEC&state=CA&limit=2")
+    parsed_the_data_how_could_i_forget = JSON.parse(one.body, symbolize_names: true)
+    binding.pry
   end
 
   def get_a_basic_connection_step_one
